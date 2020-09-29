3e1eafeedb8d003160a30ed6aaa70fc163567f94
gutaors <gutaors@gmail.com>
2020-09-29 17:18:37 -0300

    3o commit cod do minerando

--- app.py
+++ app.py
@@ -11,6 +11,7 @@
 #   streamlit run app.py
 # https://semanadatascience.com/aula1/
 # da uma olhada https://www.youtube.com/watch?v=skpiLtEN3yk
+pip install plotly.express
 import pandas as pd
 import streamlit as st
 import plotly.express as px
